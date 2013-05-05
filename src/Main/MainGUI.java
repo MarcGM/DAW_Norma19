ebbad245f3465e71d392b957e1d98ff009f91237
Marc <Marc@192.168.3.166>
2013-05-05 02:06:34 +0200

    -Pantalla principal feta.
    -Pantalla principal lincada amb la pantalla de modificacions de clients.
    
    
    
    -Falta acabar de mirar i corregir alguns errors.
    -Falta modificar la pantalla de clients amb un error que vaig coneixer fà temps.
    
    -Falta fer lo demés del programa.

--- src/Main/MainGUI.java
+++ src/Main/MainGUI.java
@@ -271,7 +271,7 @@ public class MainGUI extends JPanel {
     private javax.swing.JLabel telefonLabel;
     private org.jdesktop.beansbinding.BindingGroup bindingGroup;
     // End of variables declaration//GEN-END:variables
-    public static void main(String[] args) {
+    public void main() {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
---
+++ src/Main/Main_Norma19.java
@@ -0,0 +1,19 @@
+
+package Main;
+
+/*
+ * @author Marc
+ */
+
+public class Main_Norma19
+{
+    
+    
+    public static void main(String[] args)
+    {
+        Prin programaNorma19 = new Prin();
+        programaNorma19.setVisible(true);
+    }
+    
+    
+}
