16bd5cc4f30b622e549a453aa8bd43c5c152af46
Marc <Marc@192.168.3.166>
2013-05-06 20:48:13 +0200

    Modificat 2 textos.
    -Enllaços entre pantalles funcionen.
    -Modificacions a la part gràfica de la pantalla de modificació de la taula de clients feta.
    -Desplegable de "idPresentador" per a escollir-lo per a fer el rebut, funciona (el select de BD).
    -Desplegable de "idOrdenante" per a escollir-lo per a fer el rebut, funciona (el select de la BD).
    -Capturar opcins escullides a les llistes.
    
    -Falta fer:
    
    -Fer document Norma 19.
    
    
    
    FET!

--- src/Main/EscriureFitxerN19.java
+++ src/Main/EscriureFitxerN19.java
@@ -5,6 +5,9 @@
 package Main;
 
 import java.util.ArrayList;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 
 public class EscriureFitxerN19
@@ -14,6 +17,8 @@ public class EscriureFitxerN19
    public ArrayList<String> arrayLiniesFitxerIO;
    public String liniaTotalOrdenante;
    public String liniaTotalGeneral;
+   
+   public File fitxerDomiciliacions;
     
     public EscriureFitxerN19(String liniaCapceleraDePresentador, String liniaCapceleraDeOrdenante, ArrayList<String> arrayLiniesFitxerIO, String liniaTotalOrdenante, String liniaTotalGeneral)
     {
@@ -26,11 +31,37 @@ public class EscriureFitxerN19
     
     public void start()
     {
+        //Proves: >>>
         System.out.println(this.liniaCapceleraDePresentador);
         System.out.println(this.liniaCapceleraDeOrdenante);
         
         System.out.println(this.liniaTotalOrdenante);
         System.out.println(this.liniaTotalGeneral);
+        
+        escriureFitxerFinal();
+        
+    }
+    
+    public void escriureFitxerFinal()
+    {
+            try{
+                File fitxerDomiciliacions = new File("domiciliacions.txt");
+                if(fitxerDomiciliacions.exists()==false){
+                    fitxerDomiciliacions.createNewFile();
+                }
+                FileWriter fitxerWrite = new FileWriter(fitxerDomiciliacions);
+                fitxerWrite.write(liniaCapceleraDePresentador+"\n");
+                fitxerWrite.write(liniaCapceleraDeOrdenante+"\n");
+                for(int i=0; i<arrayLiniesFitxerIO.size(); i++){
+                    fitxerWrite.write(arrayLiniesFitxerIO.get(i)+"\n");
+                }
+                fitxerWrite.write(liniaTotalOrdenante+"\n");
+                fitxerWrite.write(liniaTotalGeneral+"\n");
+                
+                fitxerWrite.close();
+            }catch(IOException e){
+                e.printStackTrace();
+            }
     }
     
     
